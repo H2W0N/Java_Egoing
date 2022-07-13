17bc1f57aca38c5e060cc31061da4363e6b9dc48
H2W0N <woohw97@naver.com>
2022-07-13 15:06:41 +0900

    변수와 메소드

--- Object/OthersOOP.java
+++ Object/OthersOOP.java
@@ -2,7 +2,9 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class OthersOOP {
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException {
+        // class : System, Math, FileWriter
+        // instance : f1, f2
         System.out.println(Math.PI);
         System.out.println(Math.floor(1.8)); // 내림
         System.out.println(Math.ceil(1.8)); // 올림
@@ -11,5 +13,10 @@ public class OthersOOP {
         f1.write("Hello");
         f1.write(" JAVA");
         f1.close();
+
+        FileWriter f2 = new FileWriter("data2.txt");
+        f2.write("Hello");
+        f2.write(" JAVA2");
+        f2.close();
     }
 }
